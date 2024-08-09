eb143516bb24e2d928d664c9571a30f08f06b248
晨出 <1963383112@qq.com>
2024-08-10 01:16:31 +0800

    项目启动成功日志

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    Logger.log('项目启动成功，访问地址: http://localhost:3000');
+  });
 }
 bootstrap();
